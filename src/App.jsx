18d31852b2ba5e8a0d95684ad019a9fb25540205
Tiago Gil <talmeidagil@hotmail.com>
2023-09-26 08:55:54 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,7 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={tweetsArray[0]} />
-      <Tweet tweet={tweetsArray[1]} />
-      <Tweet tweet={tweetsArray[2]} />
+      {tweetsArray.map(tweet => (<Tweet tweet={tweet} />))}
     </div>
   );
 }
